d53c5b9eed8e42a708dac0e351ec7e79fd7b25af
Thong Ngo | Ntrepid Corp <thong.ngo@ntrepidcorp.com>
2019-03-06 17:11:01 -0500

    added more files

--- complete-node/playground/8-promises.js
+++ complete-node/playground/8-promises.js
@@ -5,16 +5,36 @@ const doWorkPromise = new Promise((resolve, reject) => {
     }, 2000)
 })
 
-doWorkPromise.then((result) => {
-    console.log('Success!', result)
-}).catch((error) => {
-    console.log('Error!', error)
-})
+// doWorkPromise.then((result) => {
+//     console.log('Success!', result)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
 
-//
-//                               fulfilled
-//                              /
-// Promise      -- pending --> 
-//                              \
-//                               rejected
-//
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+// add(1, 2).then((sum) => {
+//     console.log(sum)
+//     add(sum, 5).then((sum2) => {
+//         console.log(sum2)
+//     }).catch((e) => {
+//         console.log(e)
+//     })
+// }).catch((e) => {   
+//     console.log(e)
+// })
+
+add(1, 1).then((sum) => {
+    console.log(sum)
+    return add(sum, 4)
+}).then((sum2) => {
+    console.log(sum2)
+}).catch((e) => {
+    console.log(e)
+})
